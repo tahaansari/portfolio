0da0942ab1e4a6a60b75347cc67df23e26f1db2e
tahaansari <taha.m.ansari@gmail.com>
2019-09-07 15:34:14 +0530

    removed unwanted files

--- assets/js/ta.js
+++ assets/js/ta.js
@@ -1,25 +1,3 @@
 $(document).ready(function(){
-	// alert('doc is ready');
-	
-	$bannerTop = $('nav').outerHeight();
-	$('.banner-container').css('margin-top',$bannerTop);
 
-
-	function resize(){
-		if($(window).width() <= 992){
-			$('nav ul').css('top',$bannerTop);
-		}else{
-			$('nav ul').css('top','50%');
-			$('nav ul').css('transform','translateY(-50%)');
-		}
-	}
-
-	resize();
-	
-	$(window).resize(function(){resize()});
-
-
-	// $('.hamburger').click(function(){
-	// 	$('nav').toggleClass('active');
-	// })			
 })
