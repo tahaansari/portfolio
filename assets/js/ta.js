5311d76a2e2371ac1658ee417b1054902d814935
tahaansari <taha.m.ansari@gmail.com>
2019-09-14 17:14:34 +0530

    going to work on desktop

--- assets/js/ta.js
+++ assets/js/ta.js
@@ -1,3 +1,30 @@
-$(document).ready(function(){
+var options = {
+    textColour: "#202020",
+    textHeight: 20,
+    depth: 3,
+    wheelZoom:false
+};
 
-})
+var width = $(window).width();
+var height = $(window).height();
+
+window.onload = function() {
+
+    $('canvas').attr({'width':width,'height':height});
+
+    try {
+        TagCanvas.Start("skillCanvas", "", options);
+        console.log("Start skill animation");
+    } catch (e) {
+        // something went wrong, hide the canvas container
+        console.log("Hide canvas");
+        document.getElementById("skillCanvasContainer").style.display = "none";
+    }
+};
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
